f4d2fae8e96c0059befc92e0744231f276ba4db2
howtungtung <howtungtung@gmail.com>
2020-09-15 00:25:47 +0800

    update

---
+++ Assets/Scripts/ValueObject/CharacterData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CharacterData
+{
+    public int id;
+    public string name;
+    public int hp;
+    public int atk;
+    public int actionTime;
+
+    public event UnityAction<int> onHPAdd;
+    public event UnityAction<int> onHPReduce;
+
+    public void AddHP(int amount)
+    {
+        hp += amount;
+        onHPAdd?.Invoke(amount);
+    }
+
+    public void ReduceHP(int amount)
+    {
+        hp -= amount;
+        onHPReduce?.Invoke(amount);
+    }
+
+    public CharacterData Clone()
+    {
+        return (CharacterData)this.MemberwiseClone();
+    }
+}
